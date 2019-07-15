78e28fb0a194e2b8c93870827d3308c6521d2c28
yasin <yasin@google.com>
2019-07-15 12:08:53 +0430

    add main file and description

---
+++ rest-api.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * Plugin Name: Y REST API
+ * Plugin URI: https://atbox.io/yasindev
+ * Description: This Plugin To Show Your Profile in widget.
+ * Version: 1.0
+ * Author: Yasin Ti
+ * Author URI: https://atbox.io/yasintechnology
+ * License: GPLv2 or later
+ *
+ * @package   Y REST API
+ * @author    Yasin Ti <yasin.coding@gmail.com>
+ * @copyright Copyright (c) 2018, atbox.io/yasintechnology.
+ *
+ */
+
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'dont access!' );
+}
+
